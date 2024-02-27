3895a60a30dfd4de29dd2f00f79d642694a12848
MostafaAlaa297 <elaskary609@gmail.com>
2024-02-27 15:03:11 -0800

    add node right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -23,7 +23,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		parent->right->parent = new_node;
 	}
 
-	new_node->right = new_node;
+	parent->right = new_node;
 
 	return (new_node);
 }
